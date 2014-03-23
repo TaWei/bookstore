d107d6ba89cd06e35eec8c31b544b735acd3533e
Kishan Patel <patel.m.kishan@gmail.com>
2014-03-23 19:28:37 -0400

    Added queries for Q4.

--- comp421d3/src/backend/QueryExecutor.java
+++ comp421d3/src/backend/QueryExecutor.java
@@ -1,10 +1,11 @@
 package backend;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class QueryExecutor implements Queries {
+public class QueryExecutor{
 	private Connection conn;
 	private Statement stm;
 	
@@ -17,42 +18,153 @@ public class QueryExecutor implements Queries {
 		}
 	}
 	
-	@Override
-	public String addUser(String userName, String firstName, String lastName,
-			String email, String password) {
-		// TODO Auto-generated method stub
-		return null;
+	public String addUser(String userName, String firstName, String lastName, String email, String password) {
+		String sqlInsert = "insert into User values('"+userName+"','"+firstName+"','"+lastName+"','"+email+"','"+password+"')";
+		String output = null;
+		
+		try{
+			stm.execute(sqlInsert);
+		}catch(SQLException e){
+			output = "An error occured while attempting to insert -- "+e.toString();
+		}
+		
+		if(output == null){
+			output = "Insert was successfull.";
+		}
+		
+		return output;
 	}
 
-	@Override
-	public String addBook(String isbn, String title, String summary,
-			String genre, int stock) {
-		// TODO Auto-generated method stub
-		return null;
+	public String addBook(String isbn, String title, String summary, String genre, int stock) {
+		String sqlInsert = "insert into Book values('"+isbn+"','"+title+"','"+summary+"','"+genre+"',"+stock+")";
+		String output = null;
+		
+		try{
+			stm.execute(sqlInsert);
+		}catch(SQLException e){
+			output = "An error occured while attempting to insert -- "+e.toString();
+		}
+		
+		if(output == null){
+			output = "Insert was successfull.";
+		}
+		
+		return output;
 	}
 
-	@Override
-	public String getBooksGreaterThan4Stars(String userName) {
-		// TODO Auto-generated method stub
-		return null;
+	public String getBooksGreaterThanNStarsForUser(String userName, double stars) {
+		String sqlSelect  = "select b.title, avg(r.stars) as avg_stars "
+							+ "from Book as b, Reviews as r, reviewFor as rF "
+							+ "where b.isbn = rf.isbn and r.reviewId = rF.reviewId "
+							+ "and b.isbn in (select c.isbn from contains as c, makes as m where c.orderId = m.orderId and m.userName = '"+userName+"') "
+							+ "group by b.isbn, b.title "
+							+ "having avg(r.stars) >= "+stars;				
+		ResultSet result;
+		String output = "";
+		
+		try{
+			result = stm.executeQuery(sqlSelect);
+			output += "Title\t\tAvg Stars\n";
+			output += "------\t\t--------\n";
+			while(result.next()){
+			  output += result.getString("title")+"\t"+result.getString("avg_stars")+"\n";
+			}
+		}catch(SQLException e){
+			output = "An error occured while executing the query: "+e.toString();
+		}
+		System.out.println(output);
+		
+		return output;
 	}
 
-	@Override
-	public String getBestCustomers(int number) {
-		// TODO Auto-generated method stub
-		return null;
+	
+	public String getNBestCustomersForYear(int number, int year) {
+		String sqlSelect = "select m.username, count(*) as orders_made "
+							+ "from makes as m, orders as o "
+							+ "where m.orderId = o.orderId and o.orderDate >= '"+year+"-01-01 00:00:00' and o.orderDate < '"+(year+1)+"-01-01 00:00:00' "
+							+ "group by m.username "
+							+ "order by count(*) desc "
+							+ "fetch first "+number+" rows only";
+		ResultSet result;
+		String output = "";
+		
+		try{
+			result = stm.executeQuery(sqlSelect);
+			output += "Username\tOrders_Made\n";
+			output += "--------\t-----------\n";
+			while(result.next()){
+				output += result.getString("username")+"\t\t"+result.getString("orders_made")+"\n";
+			}
+		}catch(SQLException e){
+			output = "An error occured while executing the query: "+e.toString();
+		}
+		System.out.println(output);
+		
+		return output;
 	}
 
-	@Override
-	public String getMostPopularGenres(int number) {
+	public String getNMostPopularGenresForYear(int number, int year) {
 		// TODO Auto-generated method stub
-		return null;
+		String sqlSelect = "select b.genre, count(*) as orders_made "
+							+ "from book as b, contains as c, orders as o "
+							+ "where c.isbn = b.isbn and c.orderId = o.orderId and o.orderDate >= '"+year+"-01-01 00:00:00' and o.orderDate < '"+(year+1)+"-01-01 00:00:00' "
+							+ "group by b.genre "
+							+ "order by count(*) desc "
+							+ "fetch first "+number+" rows only";
+		ResultSet result;
+		String output = "";
+		
+		try{
+			result = stm.executeQuery(sqlSelect);
+			output += "Genre\t\tOrders_Made\n";
+			output += "--------\t-----------\n";
+			while(result.next()){
+				output += result.getString("genre")+"\t\t"+result.getString("orders_made")+"\n";
+			}
+		}catch(SQLException e){
+			output = "An error occured while executing the query: "+e.toString();
+		}
+		System.out.println(output);
+		
+		return output;
 	}
 
-	@Override
-	public String activateOrUpdateCoupons(String couponCode, String validUntil) {
-		// TODO Auto-generated method stub
-		return null;
+	public String activateOrUpdateCoupons(String couponCode, int yearEnd, int monthEnd, int dayEnd, String timeEnd) {
+		String sqlUpdate = "update Coupons "
+							+ "set validFrom = CURRENT_TIMESTAMP, validUntil='"+yearEnd+"-"+monthEnd+"-"+dayEnd+" "+timeEnd+"'"
+							+ "where couponCode='"+couponCode+"'";
+		String output = null;
+		
+		try{
+			stm.execute(sqlUpdate);
+		}catch(SQLException e){
+			output = "An error occured while attempting to update -- "+e.toString();
+		}
+		
+		if(output == null){
+			output = "Update was successfull.";
+		}
+		
+		return output;
+	}
+	
+	public String changeDestAddressOfActiveOrder(int orderId, int addrId) {
+		String sqlUpdate = "update Orders "
+							+ "set addrId = "+addrId
+							+"where orderId = "+orderId;
+		String output = null;
+		
+		try{
+			stm.execute(sqlUpdate);
+		}catch(SQLException e){
+			output = "An error occured while attempting to update -- "+e.toString();
+		}
+		
+		if(output == null){
+			output = "Update was successfull.";
+		}
+		
+		return output;
 	}
 	
 	public void createTestTable(){
@@ -80,6 +192,12 @@ public class QueryExecutor implements Queries {
 		
 		//To test connection - If working from home, you need to be VPN'd
 		//qe.createTestTable();
-		qe.dropTestTable();
+		//qe.dropTestTable();
+		
+		//qe.addUser("rjberfer22", "Robert", "Berger", "rberger222@gmail.com", "blagagaga");
+		//qe.addBook("977-0316015844", "Head First Design Patterns", "Design pattern books for dummies", "Computer Science", 100);
+		//qe.getBooksGreaterThanNStarsForUser("kpatel", 1);
+		//qe.getNBestCustomersForYear(10, 2014);
+		//qe.getNMostPopularGenresForYear(10, 2014);
 	}
 }
