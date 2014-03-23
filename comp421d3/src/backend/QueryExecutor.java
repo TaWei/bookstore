455943b2d5ae2131dd88dbd092f076cb30025c77
Kishan Patel <patel.m.kishan@gmail.com>
2014-03-23 15:49:49 -0400

    Adding potential queries for Q4.

--- comp421d3/src/backend/QueryExecutor.java
+++ comp421d3/src/backend/QueryExecutor.java
@@ -16,4 +16,70 @@ public class QueryExecutor implements Queries {
 			e.printStackTrace();
 		}
 	}
+	
+	@Override
+	public String addUser(String userName, String firstName, String lastName,
+			String email, String password) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String addBook(String isbn, String title, String summary,
+			String genre, int stock) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getBooksGreaterThan4Stars(String userName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getBestCustomers(int number) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getMostPopularGenres(int number) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String activateOrUpdateCoupons(String couponCode, String validUntil) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	public void createTestTable(){
+	    String createSQL = "CREATE TABLE test (id INTEGER, name VARCHAR (25)) ";
+	    try {
+			this.stm.execute(createSQL);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void dropTestTable(){
+		String dropSQL = "DROP TABLE test";
+		try {
+			this.stm.execute(dropSQL);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args){
+		DB2Connection db2Conn = new DB2Connection("jdbc:db2://db2.cs.mcgill.ca:50000/cs421", "cs421g18", "bs2014[$");
+		Connection conn = db2Conn.getConnection();
+		QueryExecutor qe = new QueryExecutor(conn);
+		
+		//To test connection - If working from home, you need to be VPN'd
+		//qe.createTestTable();
+		qe.dropTestTable();
+	}
 }
